d17167bda72a4d82a8d52d09e654730b3efe979b
stormont <stormont@gmail.com>
2015-10-24 17:49:28 -0700

    Initial revision

---
+++ src/Main.elm
@@ -0,0 +1,38 @@
+
+import Html
+import Html.Attributes
+
+
+main =
+  Html.h3
+    [ buildStyle
+        [ pointerCursorStyle
+        , noTextSelectionStyle
+        ]
+    ]
+    [ Html.text "Hello world!" ]
+
+
+buildStyle : List (List (String, String)) -> Html.Attribute
+buildStyle styleLists =
+  Html.Attributes.style <| List.concat styleLists
+
+
+(=>) = (,)
+
+
+pointerCursorStyle : List (String, String)
+pointerCursorStyle =
+  [ "cursor" => "pointer" ]
+
+
+noTextSelectionStyle : List (String, String)
+noTextSelectionStyle =
+  [ "-webkit-touch-callout" => "none"
+  , "-webkit-user-select"   => "none"
+  , "-khtml-user-select"    => "none"
+  , "-moz-user-select"      => "none"
+  , "-ms-user-select"       => "none"
+  , "-user-select"          => "none"
+  ]
+
